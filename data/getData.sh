2ebad22019abe7ff93606b3d33b9c7af7b24cd6d
Jordan Kokocinski <11jdk13@queensu.ca>
2019-11-16 13:54:17 -0500

    Modified getData script to include a positional param.

--- data/getData.sh
+++ data/getData.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/bash
 
-# demand data
-wget http://reports.ieso.ca/public/Demand/PUB_Demand_{2002..2018}.csv
+if [ $1 == "demand" ]; then
+  # demand data
+  wget http://reports.ieso.ca/public/Demand/PUB_Demand_{2002..2018}.csv
+fi
 
-# HOEP data
-wget http://reports.ieso.ca/public/PriceHOEPPredispOR/PUB_PriceHOEPPredispOR_{2002..2018}.csv
+if [ $1 == "hoep" ]; then
+  # HOEP data
+  wget http://reports.ieso.ca/public/PriceHOEPPredispOR/PUB_PriceHOEPPredispOR_{2002..2018}.csv
+fi
 
 # tempearature data
-# stuff here
+if [ $1 == "temperature" ]; then
+  for year in `seq 2005 2006`
+  do
+  	for month in `seq 1 12`
+  	do
+  		wget --content-disposition "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=5097&Year=${year}&Month=${month}&Day=14&timeframe=1&submit=Download+Data"
+  	done
+  done
+fi
